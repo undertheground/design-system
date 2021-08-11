ac75f5833008c1924808af61585e2bb81ac53583
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2021-08-11 20:54:27 +0430

    storybook react preview

---
+++ packages/react/.storybook/wishworkTheme.js
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming';
+import logo from './wish-work-logo.png';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Wish Work Design System',
+  colorPrimary: '#d1036f',
+  colorSecondary: 'deepskyblue',
+  brandUrl: 'https://wishwork.org',
+  brandImage: logo,
+});
