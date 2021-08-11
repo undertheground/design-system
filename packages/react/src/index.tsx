b6ace620913f5c91db6358c31392300a0e61da1b
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2021-08-11 10:12:43 +0430

    create vue

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -11,5 +11,5 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  * A custom Thing component. Neat!
  */
 export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+  return <div>{children || `the snozzberries taste like snozzberriesssss`}</div>;
 };
