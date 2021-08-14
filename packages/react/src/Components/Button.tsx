c70e12ef6006fb3399d8d6f0a666638660b1e959
alizadeh.amirbahador <alizadeh.amirbahador@gmail.com>
2021-08-14 17:39:50 +0430

    button css half implemented

--- packages/react/src/Components/Button.tsx
+++ packages/react/src/Components/Button.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import PropTypes from 'prop-types';
 import {MouseEvent, PropsWithChildren} from 'react'
 import styled from 'styled-components';
 
-export const StyledButton = styled.button`
-padding: ${(props: PaddingCSS) => (props.size === SIZES.LARGE ? '1rem 4.86rem' : '0.8rem 2.2rem')};
-border-radius:0.3rem;
-color:#fff;
-font-size:1rem;
-font-family:'Arial';
-cursor:pointer;
-border:  ${(props: BorderLine) => (props.kind === KIND.GHOST ? '1rem solid #1C5BC2' : 'none')};
-Background:#d1036f;
-`;
+const COLOR ={
+  $WHITE:'#fff',
+  $SEXY_PINK: '#d1036f',
+  $MAROON: '#BE0064',
+  $JAZZ_BERRY:'#90004C',
+  $WISH_BLUE: '#1C5BC2',
+  $GRAY10: '#F5F5F5',
+  $GRAY15:'#F5F5F5',
+  $GRAY20:'#BFBFBF',
+  $SAPPHIRE: '#1A53AE',
+  $DARK_CORNFLOWER_BLUE: '#16438B',
+
+}
 
 const SIZES = {
   SMALL: 'small',
   MEDIUM: 'medium',
-  LARGE: 'large'
+  LARGE: 'large',
 };
 
 const KIND = {
@@ -26,43 +28,157 @@ const KIND = {
   GHOST: 'ghost',
 }
 
+const ICON = {
+  WITHOUT_ICON: 'without-icon',
+  WITH_ICON: 'with-icon',
+  ICON_ONLY: 'icon-only',
+}
+
+
+export interface ButtonPropsWithoutChildren {
+  kind?: 'primary' | 'secondary' | 'ghost',
+  size?: 'small' | 'medium' | 'large',
+  icon?: 'without-icon' | 'with-icon' | 'icon-only',
+  disabled?: boolean, // ???
+  className?: string // ???
+  style?: object // ???
+  onClick?: OnClickAdapter<HTMLButtonElement> // ???
+}
+
 type OnClickAdapter<E extends HTMLElement> = (event: MouseEvent<E>) => void
 
-interface PaddingCSS{
-  size: string;
-}
-interface BorderLine{
+//Button Styles//
+export const StyledButton = styled.button<ButtonPropsWithoutChildren>`
+font-size:1rem;
+font-family:'Arial';
+font-weight:500;
+cursor:pointer;
+border-radius:0.3rem;
+outline:none !important;
+transition: all 150ms ease-out;
+transform: translate3d(0, 0, 0);
+padding: ${(props) => 
+  (props.size === SIZES.LARGE
+    ?
+    '1rem 4.86rem'
+    :
+    props.size === SIZES.SMALL
+    ?
+    '0.8rem 2.2rem'
+    :
+    '1.1rem 2.1rem' // SIZES.MEDIUM (Default value for undefind size)
+)};
+
+
+${(props) => {
+  switch (props.kind) {
+
+    case KIND.SECONDARY:
+    
+      return(`
+        color: ${COLOR.$WISH_BLUE};
+        border:0.1rem solid ${COLOR.$WISH_BLUE};
+        background-color:${COLOR.$WHITE};
+        
+        &:hover{
+          background:${COLOR.$SAPPHIRE};
+          color: ${COLOR.$WHITE};
+          border:0.1rem solid ${COLOR.$SAPPHIRE};
+        }
+        &:active{
+          background:${COLOR.$DARK_CORNFLOWER_BLUE};
+          color: ${COLOR.$WHITE};
+          border:0.1rem solid ${COLOR.$DARK_CORNFLOWER_BLUE};
+        }`) 
+
+    case KIND.GHOST:
+    
+      return (`
+        color: ${COLOR.$SEXY_PINK};
+        background-color:${COLOR.$WHITE};
+        border:0.1rem solid ${COLOR.$WHITE} ;
+        
+        &:hover{
+          background:${COLOR.$GRAY15};
+          border:0.1rem solid ${COLOR.$GRAY15} 
+        }
+        &:active:{
+          
+          border:0.1rem solid ${COLOR.$SEXY_PINK} 
+        }`)
+
+    default: // props.kind === KIND.PRIMARY (default kind)
+
+      return (`
+        border:0.1rem solid ${COLOR.$SEXY_PINK};
+        background-color: ${COLOR.$SEXY_PINK};
+        color:${COLOR.$WHITE};
+        
+        &:hover{
+          background: ${COLOR.$MAROON};
+          border:0.1rem solid ${COLOR.$MAROON} 
+        }
+        &:active:{
+          background: ${COLOR.$JAZZ_BERRY};
+          border:0.1rem solid ${COLOR.$JAZZ_BERRY} 
+        }`)
+    
+      }   
+    } 
+  }
+
+
+
+${(props) =>{ 
+  if (!props.disabled) {
+    return ``
+  }
+  const commonStyleForDisabled = (`
+        color:${COLOR.$GRAY20};
+        cursor: not-allowed !important;
+        &:hover{
+          transform: none !important;
+        }
+        `)
+  switch (props.kind) {
+    
+    case KIND.SECONDARY:
+      return(commonStyleForDisabled + 
+        `
+        border:0.1rem solid ${COLOR.$GRAY20};
+
+        `)
+
+    case KIND.GHOST:
+      return ( commonStyleForDisabled + `
+        border:0;
+      `
+      )
+    default:
+      return(commonStyleForDisabled + `
+        background:${COLOR.$GRAY15};
+        border:0.1rem solid ${COLOR.$GRAY20};  
+      `)
+
+      
+  }
   
 }
-
-interface ButtonPropsWithoutChildren {
-  kind: 'primary' | 'secondary' | 'ghost'
-  size: 'small' | 'medium' | 'large'
-  disabled?: boolean
-  icon?: 'with-icon' | 'icon-only'  
-  onClick(): OnClickAdapter<HTMLButtonElement>
 }
+
+`;
 
 export type ButtonProps = PropsWithChildren<ButtonPropsWithoutChildren>;
 
-const buttonStyleProps = {
-  kind: PropTypes.oneOf(Object.values(KIND)),
-  size: PropTypes.oneOf(Object.values(SIZES)),
-};
-
 export const Button = (props: ButtonProps) => {
-  const {
-    kind,
-    icon,
-    size,
-    disabled,
-    children,
-    ...restOfProps
-  } = props;
+  
         return (
         <StyledButton
-          size={props.size}
-          {...restOfProps}
+        style={props.style}
+        className={props.className}
+        onClick={props.onClick}
+        disabled={props.disabled}
+          {...props}
         >
           {props.children}
         </StyledButton>
@@ -70,8 +186,6 @@ export const Button = (props: ButtonProps) => {
 }
 
 
-Button.propTypes = {
-      ...buttonStyleProps,
-      children: PropTypes.node.isRequired,
-};
- 
+
+
+
--- packages/react/stories/Components/Button.stories.tsx
+++ packages/react/stories/Components/Button.stories.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Button, ButtonProps } from '../../src/Components/Button';
+import { Button, ButtonProps, ButtonPropsWithoutChildren } from '../../src/Components/Button';
 import { Story } from '@storybook/react'
 import {MouseEvent, PropsWithChildren} from 'react'
 
 
 
-type OnClickAdapter<E extends HTMLElement> = (event: MouseEvent<E>) => void
+// type OnClickAdapter<E extends HTMLElement> = (event: MouseEvent<E>) => void
 
 export default {
   title: 'Components/Button',
@@ -14,30 +14,68 @@ export default {
     
   },
 };
-interface Props {
-    kind: 'primary' | 'secondary' | 'ghost',
-    icon?: 'with-icon' | 'icon-only',
-    size: 'small' | 'medium' | 'large',
-    disabled?: boolean,
-    children: string,
-    onClick(): OnClickAdapter<HTMLButtonElement>,
+
+// interface Props {
+//     kind: 'primary' | 'secondary' | 'ghost',
+//     icon?: 'with-icon' | 'icon-only',
+//     size: 'small' | 'medium' | 'large',
+//     // disabled?: boolean,
+//     children: string,
+//     onClick(): OnClickAdapter<HTMLButtonElement>,
+// }
 
 
-    
-}
-const Template: Story <Props> = args => <Button {...args} />;
+const Template: Story <ButtonProps> = args => <Button {...args} />;
 
-export const primary = Template.bind({});
-primary.args = {
+export const Primary = Template.bind({});
+Primary.args = {
     kind: 'primary',
-    size: 'large',
+    size: 'medium',
     children: 'Button'
 } as ButtonProps;
 
-export const secondary = Template.bind({});
-primary.args = {
+export const Secondary = Template.bind({});
+Secondary.args = {
     kind: 'secondary',
-    size: 'large',
+    size: 'medium',
     children: 'Button'
 } as ButtonProps;
 
+
+export const Ghost = Template.bind({});
+Ghost.args = {
+    kind: 'ghost',
+    size: 'medium',
+    children: 'Button'
+} as ButtonProps;
+
+export const Primary_Disabled = Template.bind({});
+Primary_Disabled.args = {
+    disabled: true,
+    kind: 'primary',
+    size: 'medium',
+    children: 'Button',
+} as ButtonProps;
+
+export const Secondary_Disabled = Template.bind({});
+Secondary_Disabled.args = {
+    disabled: true,
+    kind: 'secondary',
+    size: 'medium',
+    children: 'Button',
+    style:{},
+    className:'abas'
+} as ButtonProps;
+
+export const Ghost_Disabled = Template.bind({});
+Ghost_Disabled.args = {
+    disabled: true,
+    kind: 'ghost',
+    size: 'medium',
+    children: 'Button',
+    style:{},
+    className:'abas'
+} as ButtonProps;
+
+
+<Button kind={'primary'} children={'abas'} disabled={true} className={'abas'} style={{backgroundColor: 'black'}} /> 
