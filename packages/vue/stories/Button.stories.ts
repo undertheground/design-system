042544211ecf7b0371c35fafbe5c410e3df2f95c
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2021-08-23 18:57:54 +0430

    vue js initialze completely with styled-component

---
+++ packages/vue/stories/Button.stories.ts
@@ -0,0 +1,40 @@
+import Button from "../src/Components/Button";
+
+export default {
+  title: "Example/Button",
+  component: Button
+};
+
+const Template = args => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { Button },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<Button v-bind="args" />'
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  children: "Button"
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Button"
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+  label: "Button"
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+  label: "Button"
+};
