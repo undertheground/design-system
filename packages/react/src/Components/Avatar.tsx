b2036193d4777f2741eb7d79bb6c894adf035330
morteza-k-a <68350615+morteza-k-a@users.noreply.github.com>
2021-10-18 22:10:38 +0330

    added styles

--- packages/react/src/Components/Avatar.tsx
+++ packages/react/src/Components/Avatar.tsx
@@ -1,26 +1,48 @@
 import React, { ReactChild } from 'react';
 import styled from 'styled-components';
 
-export interface AvatarProps {
+interface AvatarFunctionalProps {
   id?: string;
   children?: ReactChild | ReactChild[];
   size?: 'S' | 'M' | 'L' | 'XL';
+  shape?: 'round' | 'square';
 }
-
+interface AvataraestheticProps {}
+export type AvatarProps = AvatarFunctionalProps & AvataraestheticProps;
 interface StyledAvatarProps {
-  size?: number;
+  size: number;
+  shape?: 'round' | 'square';
+  fontSize: number;
 }
 
 export function Avatar(props: AvatarProps) {
-  const sizeDefinition = { S: 24, M: 32, L: 40, XL: 64 };
+  const sizeDefinition = { S: 1.5, M: 2, L: 2.5, XL: 4 };
+  const FontsizeDefinition = { S: 12, M: 14.4, L: 19.2, XL: 25.6 };
   return (
     <StyledAvatar
       id={props.id}
-      size={props.size ? sizeDefinition[props.size] : undefined}
+      size={props.size ? sizeDefinition[props.size] : 2.5} //we must have a defult size
+      shape={props.shape}
+      fontSize={props.size ? FontsizeDefinition[props.size] : 19.2}
     >
       {props.children}
     </StyledAvatar>
   );
 }
 
-const StyledAvatar = styled.div<StyledAvatarProps>``;
+const StyledAvatar = styled.div<StyledAvatarProps>`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 500;
+  line-height: ${(props) => props.size + 'rem'};
+  font-size: ${(props) => props.fontSize + `px`};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #ffffff;
+  background-color: #1d62d1;
+  width: ${(props) => props.size + 'rem'};
+  height: ${(props) => props.size + 'rem'};
+  border-radius: ${(props) =>
+    props.shape === 'round' ? props.size / 2 + `rem` : `2px`};
+`;
