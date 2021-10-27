b4aae942db665b47774004dfb6476bdffa83f2c5
morteza-k-a <68350615+morteza-k-a@users.noreply.github.com>
2021-10-27 13:04:17 +0330

    added elements

--- packages/react/src/Components/Accordion.tsx
+++ packages/react/src/Components/Accordion.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { ReactChild } from 'react';
+import styled from 'styled-components';
 
-interface AccordionFunctionalProps {}
+interface AccordionFunctionalProps {
+  id?: string;
+  children?: ReactChild | ReactChild[];
+  placeholder?: string;
+}
 interface AccordionaestheticProps {}
 export type AccordionProps = AccordionFunctionalProps & AccordionaestheticProps;
 
-export function Accordion(props:AccordionProps) {
-    console.log(props)
-  return <div></div>;
+export function Accordion(props: AccordionProps) {
+  console.log(props);
+  return (
+    <AccordionWrapper id={props.id}>
+      <AccordionHeader>
+        <Icon IconName={'expand_more'} />
+        {props.placeholder}
+      </AccordionHeader>
+      <AccordionContent>{props.children}</AccordionContent>
+    </AccordionWrapper>
+  );
 }
+
+function Icon(props: { IconName?: 'expand_more' | 'close' }) {
+  return (
+    <AccordionIcon color={'#ED037C'}>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <span className={'material-icons'}>{props.IconName}</span>
+    </AccordionIcon>
+  );
+}
+
+const AccordionIcon = styled.div<{ color?: string }>``;
+const AccordionWrapper = styled.div``;
+const AccordionHeader = styled.header``;
+const AccordionContent = styled.main``;
