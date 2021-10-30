408313f843b571e65055662e218d10f2979abb5a
morteza-k-a <68350615+morteza-k-a@users.noreply.github.com>
2021-10-30 16:04:31 +0330

    added elements and icons

--- packages/react/src/Components/Checkbox.tsx
+++ packages/react/src/Components/Checkbox.tsx
@@ -1,24 +1,57 @@
 import React, { MouseEventHandler } from 'react';
 import styled from 'styled-components';
+import { white, grey73 } from '@undertheground/color/dist/color';
 
 export interface CheckboxProps {
   checked?: boolean;
   onClick?: MouseEventHandler<HTMLSpanElement>;
   placeholder?: string;
+  disabled?: boolean;
 }
 
 export function Checkbox(props: CheckboxProps) {
   console.log(props);
   return (
     <CheckBoxContainer>
-      <CheckBoxToggler checked={props.checked} onClick={props.onClick} />
+      <CheckBoxToggler
+        checked={props.checked}
+        onClick={!props.disabled ? props.onClick : undefined}
+      >
+        {props.checked && <Checked disabled={props.disabled} />}
+      </CheckBoxToggler>
       <CheckBoxText>{props.placeholder}</CheckBoxText>
     </CheckBoxContainer>
   );
 }
 
+function Checked(props: { disabled?: boolean }) {
+  return (
+    <StyledCheck backgroundColor={props.disabled ? grey73 : '#1D62D1'}>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <span style={{ fontSize: '0.75rem' }} className={'material-icons'}>
+        done
+      </span>
+    </StyledCheck>
+  );
+}
+
 const CheckBoxContainer = styled.div`
   display: flex;
+`;
+const StyledCheck = styled.div<{ backgroundColor?: string }>`
+  position: relative;
+  top: -0.5px;
+  left: -0.5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${white};
+  background-color: ${(props) => props.backgroundColor};
+  height: calc(1rem - 1px);
+  width: calc(1rem - 1px);
 `;
 const CheckBoxToggler = styled.span<{ checked?: boolean }>`
   cursor: pointer;
@@ -27,13 +60,8 @@ const CheckBoxToggler = styled.span<{ checked?: boolean }>`
   border: 1px solid #bababa;
   box-sizing: border-box;
   border-radius: 2px;
-  ${(props) => {
-    if (props.checked) return `background: #1D62D1;`;
-    else return `background: #ffffff;`;
-  }}
-  &:hover{
-    border: 1px solid #1D62D1;
+  &:hover {
+    border: 1px solid #1d62d1;
   }
-
 `;
 const CheckBoxText = styled.label``;
